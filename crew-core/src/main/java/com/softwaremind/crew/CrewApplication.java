e985070d422bd206089d06c97406373c1172078d
Dariusz Babiarz <dariusz.babiarz@softwaremind.com>
2018-04-06 08:57:14 +0200

    Initial commit - project structure

---
+++ crew-core/src/main/java/com/softwaremind/crew/CrewApplication.java
@@ -0,0 +1,18 @@
+package com.softwaremind.crew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Crew application entry point class.
+ *
+ * @author daba
+ * @since 06.04.2018
+ */
+@SpringBootApplication
+public class CrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewApplication.class, args);
+    }
+}
--- crew-core/src/test/java/com/softwaremind/crew/CrewApplicationTests.java
+++ crew-core/src/test/java/com/softwaremind/crew/CrewApplicationTests.java
@@ -1,16 +1,19 @@
-package com.softwaremind.crewapp;
+package com.softwaremind.crew;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * TestSuit for {@link CrewApplication} class.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CrewAppApplicationTests {
+public class CrewApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
