b00555aaeed3bccd831f356e0c22a7d152aa06a6
Dariusz Babiarz <dariusz.babiarz@softwaremind.com>
2018-04-06 08:22:04 +0200

    Initial commit - project structure

---
+++ src/main/java/com/softwaremind/crewapp/CrewAppApplication.java
@@ -0,0 +1,12 @@
+package com.softwaremind.crewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewAppApplication.class, args);
+	}
+}
