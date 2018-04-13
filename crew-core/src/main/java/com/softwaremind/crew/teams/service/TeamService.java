b16938252b0fde350303767b67c05e88dc346980
mtmi <mtmi@softwaremind.com>
2018-04-13 09:53:48 +0200

    REST API - basic structure corrected

--- crew-core/src/main/java/com/softwaremind/crew/teams/service/TeamService.java
+++ crew-core/src/main/java/com/softwaremind/crew/teams/service/TeamService.java
@@ -2,16 +2,21 @@ package com.softwaremind.crew.teams.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.softwaremind.crew.teams.model.TeamModel;
 import com.softwaremind.crew.teams.repository.TeamRepository;
 
+/**
+ * Team service class
+ */
 @Service
 public class TeamService {
 	
 	private final TeamRepository teamRepository;
-	
+
+	@Autowired
 	public TeamService() {
 		teamRepository = new TeamRepository();
 	}
@@ -23,9 +28,9 @@ public class TeamService {
 	/**
 	 * Initialize repository method which returns list
 	 * 
-	 * @return
+	 * @return list of teams
 	 */
-	public List<TeamModel> getmyteams() {
+	public List<TeamModel> findAll() {
 		return teamRepository.listStart();
 	}
 }
