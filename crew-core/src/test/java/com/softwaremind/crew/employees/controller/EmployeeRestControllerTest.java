ed4143090902f18a62115b731b23bb0ced526063
wire <wire@softwaremind.com>
2018-04-16 13:45:45 +0200

    Issue: 1 Comment:REST API - implement employees endpoint Corrected javadoc, improvement using Mockito and Controller

--- crew-core/src/test/java/com/softwaremind/crew/employees/controller/EmployeeRestControllerTest.java
+++ crew-core/src/test/java/com/softwaremind/crew/employees/controller/EmployeeRestControllerTest.java
@@ -1,8 +1,7 @@
 package com.softwaremind.crew.employees.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.Arrays;
 
@@ -16,7 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.softwaremind.crew.employees.model.Employee;
@@ -50,12 +48,12 @@ public class EmployeeRestControllerTest {
 		mockMvc.perform(get("/employees"))
 				.andExpect(status().isOk())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(e1.getId()))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value(e1.getFirstName()))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value(e1.getLastName()))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].location").value(e1.getLocation()))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value(e1.getEmail()))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].department").value(e1.getDepartment()))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].role").value(e1.getRole()));
+				.andExpect(jsonPath("$[0].id").value(e1.getId()))
+				.andExpect(jsonPath("$[0].firstName").value(e1.getFirstName()))
+				.andExpect(jsonPath("$[0].lastName").value(e1.getLastName()))
+				.andExpect(jsonPath("$[0].location").value(e1.getLocation()))
+				.andExpect(jsonPath("$[0].email").value(e1.getEmail()))
+				.andExpect(jsonPath("$[0].department").value(e1.getDepartment()))
+				.andExpect(jsonPath("$[0].role").value(e1.getRole()));
 	}
 }
