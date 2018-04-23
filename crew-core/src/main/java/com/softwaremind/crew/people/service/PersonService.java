b8b0246031791cbe3733fcc3d0f3958d54a55c1b
mtmi <mtmi@softwaremind.com>
2018-04-23 10:50:09 +0200

    Revert "REST API - add Person Jpa ention, implements h2 database, add mapper and create Person DTO, fixed
    This reverts commit b169382

--- crew-core/src/main/java/com/softwaremind/crew/people/service/PersonService.java
+++ crew-core/src/main/java/com/softwaremind/crew/people/service/PersonService.java
@@ -1,6 +1,5 @@
 package com.softwaremind.crew.people.service;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -8,7 +7,6 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.softwaremind.crew.people.model.Person;
 import com.softwaremind.crew.people.model.dto.PersonDto;
 import com.softwaremind.crew.people.repository.PersonRepository;
 
@@ -30,20 +28,10 @@ public class PersonService {
 	 * @since 20.04.2018
 	 */
 	ModelMapper modelMapper = new ModelMapper();
-
+	
 	@Autowired
 	public PersonService(PersonRepository personRepository) {
 		this.personRepository = personRepository;
-	}
-	
-	public PersonDto mapPersonToPersonDto(Person person) {
-		PersonDto personDto = modelMapper.map(person, PersonDto.class);
-		return personDto;
-	}
-	
-	public Person mapDtoToPersonEntity(PersonDto personDto) {
-		Person person = modelMapper.map(personDto, Person.class);
-		return person;
 	}
 	
 	/**
