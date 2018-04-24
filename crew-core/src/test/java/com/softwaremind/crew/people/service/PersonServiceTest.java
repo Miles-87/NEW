5dadd80f1e105591bc1574632550a4f491b3d7b3
mtmi <mtmi@softwaremind.com>
2018-04-24 14:21:12 +0200

    Revert "REST API - add Person Jpa ention, implements h2 database, add mapper and create Person DTO, fixed
    This reverts commit b169382

--- crew-core/src/test/java/com/softwaremind/crew/people/service/PersonServiceTest.java
+++ crew-core/src/test/java/com/softwaremind/crew/people/service/PersonServiceTest.java
@@ -38,7 +38,7 @@ public class PersonServiceTest {
 	}
 	
 	@Before
-	private void initMockRepositoryToTest() {
+	public void initMockRepository() {
 		Person person1 = new Person(1L, "jan", "mucha", "krakow", "email1@onet.com", "Programing", "Developer");
 		Person person2 = new Person(3L, "Alicja", "Kowalska", "Warszawa", "email2@gmail.com", "Business", "Designer");
 		Mockito.when(personRepository.findAll()).thenReturn(Arrays.asList(person1, person2));
@@ -46,7 +46,6 @@ public class PersonServiceTest {
 	
 	@Test
 	public void shouldReturnAllPeople() {
-		initMockRepositoryToTest();
 		
 		List<PersonDto> resultPeopleDtos = personService.findAll();
 		assertThat(resultPeopleDtos).hasSize(2);
@@ -54,9 +53,8 @@ public class PersonServiceTest {
 	
 	@Test
 	public void shouldReturnPersonById() {
-		initMockRepositoryToTest();
 		
-		Optional<PersonDto> result = personService.findById(3l);
+		Optional<PersonDto> result = personService.findById(3L);
 		assertThat(result).isNotNull();
 	}
 	
