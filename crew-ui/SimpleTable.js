fa7cad32b939111be5d5ce61c74132bc39c1540a
wire <wire@softwaremind.com>
2018-05-22 10:19:01 +0200

    Issue 16: WIP: Resolve "UI for managing Teams Comment: Creating basic table for Team using Material-UI and React.js

---
+++ crew-ui/SimpleTable.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+{/*
+   @author Wiktor Religo
+ * @since 22.05.2018*/
+}
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+    head: {
+        backgroundColor: '#ff6666',
+        color: 'white',
+        fontSize: '20px',
+
+    },
+    innerRow: {
+        backgroundColor: '#ffffc7',
+        fontSize: '15',
+        fontWeight: 'bold',
+    }
+});
+
+let id = 0;
+
+function createData(name, city, description, headcount) {
+    id += 1;
+    return {id, name, city, description, headcount};
+}
+
+const data = [
+    createData("Cebule z Polski", "Kraków", "Kilka danych o drużynie ", 12),
+    createData("Pierogi jak u Mamy", "Rzeszów", "Kilka danych odrużynie", 15),
+    createData("Koksy z Huty", "Kraków", "Kilka danych odrużynie", 32),
+    createData("Wiemy że nie wiemy", "Warszawa", "Kilka danych odrużynie", 11),
+    createData("Grubi Anorektycy", "Poznań", "Kilka danych odrużynie", 56),
+];
+
+
+function SimpleTable(props) {
+    const {classes} = props;
+    return (
+        <Paper className={classes.root}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell className={classes.head}>ID:</TableCell>
+                        <TableCell className={classes.head} numeric>Name:</TableCell>
+                        <TableCell className={classes.head} numeric>City</TableCell>
+                        <TableCell className={classes.head} numeric>Description</TableCell>
+                        <TableCell className={classes.head} numeric>Headcount</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map(n => {
+                        return (
+                            <TableRow key={n.id}>
+                                <TableCell className={classes.innerRow} component="th" scope="row">
+                                    {n.id}
+                                </TableCell>
+                                <TableCell className={classes.innerRow} numeric>{n.name}</TableCell>
+                                <TableCell className={classes.innerRow} numeric>{n.city}</TableCell>
+                                <TableCell className={classes.innerRow} numeric>{n.description}</TableCell>
+                                <TableCell className={classes.innerRow} numeric>{n.headcount}</TableCell>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+}
+
+SimpleTable.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(SimpleTable);
---
+++ crew-ui/Teams.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Rout} from 'react-router';
+import SimpleTable from './SimpleTable.js';
+
+
+{/*
+   @author Wiktor Religo
+ * @since 21.05.2018*/
+}
+
+class Teams extends React.Component {
+    render() {
+        return (
+            <div>
+                <SimpleTable/>
+            </div>
+        );
+    }
+}
+
+export default Teams;
